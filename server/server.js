400c9c66b0a4cf7ab1bab69c77ef7ab108f589df
Jaime Daza <jaimdaza@gmail.com>
2020-07-20 20:31:34 -0400

    Comentarios nuevos

--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
 require('./config/config.js')
 const express = require('express')
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // se usa para obtener objetos que vengan en la peticion
 
 // parse application/x-www-form-urlencoded son funciones
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json son funciones
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false })) // se usa para obtener objetos que vengan en la peticion
+    // parse application/json son funciones
+app.use(bodyParser.json()) // se usa para obtener objetos que vengan en la peticion
 
 app.get('/usuario', function(req, res) {
     res.json('get usuario');
 });
 
 app.post('/usuario', function(req, res) {
-    let body = req.body;
+    let body = req.body; // se obtiene el objeto que viene en peticion
     res.json({ persona: body });
 });
 
 app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
+    let id = req.params.id; // se obtiene el parametro que viene por URL, id
     res.json('put usuario' + id);
 });
 
