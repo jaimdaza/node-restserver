4de82912d9d7e6a020844e0708f884e38df13d07
Jaime Daza <jaimdaza@gmail.com>
2020-07-29 20:27:20 -0400

    variable de entorno MG

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://admin:admin@cluster0.e1mzw.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
